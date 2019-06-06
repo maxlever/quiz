48bee281a93d5bbcf546e9511b3a5108db8b8f22
Max Lever <maxdavidlever@gmail.com>
2019-06-05 21:48:17 -0400

    add basic API query as serverless function

---
+++ functions/query.js
@@ -0,0 +1,44 @@
+// const fs = require('fs');
+// fs.readFileSync('../src/templates/data/survey.graphql')
+const request = require('request');
+const query = `
+{
+  questions: getQuestionList {
+    questions: items {
+      text
+      position
+      id: _id
+      choices {
+        id: _id
+        text
+        position
+      }
+      correctChoice {
+        id: _id
+      }
+    }
+  }
+}
+`;
+
+const PROJECT_ID = process.env.TS_PROJECT_ID;
+const API_KEY = process.env.TS_AUTH_TOKEN;
+
+exports.handler = function (event, context, callback) {
+
+  request.post(`https://api.takeshape.io/project/${PROJECT_ID}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`
+    },
+    body: JSON.stringify({query})
+  }).then(res => {
+    return res.json();
+  }).then(json => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(json)
+    });
+  });
+}
